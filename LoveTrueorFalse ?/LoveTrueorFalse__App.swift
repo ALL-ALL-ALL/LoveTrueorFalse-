59e5000fd13b1d1af14fec15d9379347a9b94ff3
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-15 02:01:31 +0100

    Initial Commit

---
+++ LoveTrueorFalse ?/LoveTrueorFalse__App.swift
@@ -0,0 +1,17 @@
+//
+//  LoveTrueorFalse__App.swift
+//  LoveTrueorFalse ?
+//
+//  Created by  Ixart on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoveTrueorFalse__App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
