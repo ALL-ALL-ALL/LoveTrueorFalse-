e9890090d54b7b5928137b413da9d8e3413c61ca
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-17 02:28:40 +0100

    fin✅

--- LoveTrueorFalse ?/ContentView.swift
+++ LoveTrueorFalse ?/ContentView.swift
@@ -16,14 +16,98 @@ struct ContentView: View {
                     Image("amelia")
                             .resizable()
                             .aspectRatio(contentMode: .fill)
-                            .frame(width: 300, height: 320)
+                            .frame(width: 300, height: 330)
                             .edgesIgnoringSafeArea(.all)
-                        .padding(.top,-110)
-                    Text("Amelia")
+                            .padding(.top,-110)
+                    Image("amelia")
+                            .resizable()
+                            .aspectRatio(contentMode: .fill)
+                            .frame(width: 100, height: 130)
+                            .clipShape(Circle())
+                            .padding(.leading,-180)
+                            .padding(.top,-120)
+
+
+                       
+                    Text("Amelia George ")
                         .foregroundStyle(.white)
                         .bold()
-                    Text("George")
+                        .font(.title)
+                        .padding(.leading,90)
+                        .padding(.top,-70)
+                
+                    Text("Singer ")
                         .foregroundStyle(.white)
+                        .padding(.leading,-50)
+                        .padding(.top,-35)
+                
+                    Text("Followers")
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(.leading,-35)
+                        .padding(.top,35)
+                
+                    Text("Following")
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(.leading,150)
+                        .padding(.top,35)
+                
+                    Text("Post")
+                        .bold()
+                        .foregroundStyle(.white)
+                        .padding(.leading,310)
+                        .padding(.top,35)
+                    
+                
+                
+                    Image(systemName: "music.note.list")
+                        .foregroundStyle(.white)
+                        .bold()
+                        .font(.system(size: 20))
+                        .padding(.leading,-190)
+                        .padding(.top,140)
+
+                    Image(systemName: "bird")
+                        .foregroundStyle(.white)
+                        .bold()
+                        .font(.system(size: 20))
+                        .padding(.leading,-150)
+                        .padding(.top,140)
+
+                
+                
+                    Image(systemName: "person.2")
+                        .foregroundStyle(.white)
+                        .bold()
+                        .font(.system(size: 20))
+                        .padding(.leading,-110)
+                        .padding(.top,140)
+                
+                
+                    
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                     
             } // hstack
             
